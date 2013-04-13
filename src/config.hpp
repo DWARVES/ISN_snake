356601bf4814b0b742fea7e0c2ad7150572bda03
lucas8 <luc.linux@mailoo.org>
2013-04-13 19:49:25 +0200

    Ajout de onstantes dans config.hpp.

--- src/config.hpp
+++ src/config.hpp
@@ -5,6 +5,9 @@
 const char* rcdir = "rc/";
 const char* bonus_subdir = "bonus/"; // bonus_dir = rcdir/bonus_subdir
 const char* bg_path = "bg.png";
+const unsigned int widthMap = 80;
+const unsigned int heightMap = 45;
+const unsigned int sizeTile = 20;
 
 #endif
 
