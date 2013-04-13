55d9a8bbcbdc9100314e54bd1f7026a8045f4bdd
lucas8 <luc.linux@mailoo.org>
2013-04-13 21:10:49 +0200

    Utilisation de define pour config.hpp : corrige définition multiple.

--- src/config.hpp
+++ src/config.hpp
@@ -2,12 +2,15 @@
 #ifndef DEF_CONFIG
 #define DEF_CONFIG
 
-const char* rcdir = "rc/";
-const char* bonus_subdir = "bonus/"; // bonus_dir = rcdir/bonus_subdir
-const char* bg_path = "bg.png";
-const unsigned int widthMap = 80;
-const unsigned int heightMap = 45;
-const unsigned int sizeTile = 20;
+// Ressources
+#define rcdir "rc/"
+#define bonus_subdir "bonus/" // bonus_dir rcdir/bonus_subdir
+#define bg_path "bg.png"
+
+// Map
+#define widthMap 80
+#define heightMap 45
+#define sizeTile 20
 
 #endif
 
