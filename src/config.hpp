2ab289b7731c89f221b1206c1d549769f17cc9e6
lucas8 <luc.linux@mailoo.org>
2013-04-13 18:09:28 +0200

    Ajout d'un header de configuration.

---
+++ src/config.hpp
@@ -0,0 +1,10 @@
+
+#ifndef DEF_CONFIG
+#define DEF_CONFIG
+
+const char* rcdir = "rc/";
+const char* bonus_subdir = "bonus/"; // bonus_dir = rcdir/bonus_subdir
+const char* bg_path = "bg.png";
+
+#endif
+
