9952b5396265992e94dc3f012a9dce4bada9e6dc
lucas8 <luc.linux@mailoo.org>
2013-04-13 22:02:58 +0200

    Correction de l'usage des itérateurs dans Map.

--- src/map.cpp
+++ src/map.cpp
@@ -60,7 +60,7 @@ void Map::addBonus()
 		if(prob <= state)
 		{
 			addBonus(x, y, i);
-			i = bonus.size();
+			return;
 		}
 	}
 }
@@ -87,11 +87,15 @@ void Map::blitOn(SDL_Surface* dst, SDL_Rect* pos)
 		SDL_BlitSurface(m_bg, NULL, dst, &rpos);
 
 	Uint32 timeSpent = SDL_GetTicks() - m_ltime;
-	for(bon_it it = m_bonus.begin(); it != m_bonus.end(); ++it)
+	for(bon_it it = m_bonus.begin(); it != m_bonus.end();)
 	{
 		// Mise à jour des valeurs
 		if(timeSpent > it->timeleft)
-			deleteBonus(it);
+		{
+			bon_it toErase = it;
+			++it;
+			deleteBonus(toErase);
+		}
 		else
 		{
 			it->timeleft -= timeSpent;
@@ -99,6 +103,7 @@ void Map::blitOn(SDL_Surface* dst, SDL_Rect* pos)
 			rpos.x = it->x * sizeTile;
 			rpos.y = it->y * sizeTile;
 			SDL_BlitSurface(bonus[it->idx]->getImg(), NULL, dst, &rpos);
+			++it;
 		}
 	}
 
@@ -117,7 +122,9 @@ void Map::addBonus(unsigned int x, unsigned int y, unsigned int bon)
 	ibon.x = x; ibon.y = y;
 	ibon.idx = bon;
 	ibon.timeleft = bonus[bon]->getTime();
-	m_bonus.push_back(ibon);
+
+	m_map[x][y].bon = m_bonus.insert(m_bonus.end(), ibon);
+	m_map[x][y].col = BONUS;
 }
 
 
