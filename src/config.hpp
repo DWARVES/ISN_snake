fef3b94274e08c5a6e2f392f3702fb7697fb97bf
lucas8 <luc.linux@mailoo.org>
2013-04-21 23:32:11 +0200

    Ajout d'information sur les masques dans config.hpp

--- src/config.hpp
+++ src/config.hpp
@@ -15,5 +15,18 @@
 // Snakes
 #define snake_subdir "snake/1.png"
 
+// SDL
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+#define rmask 0xff000000
+#define gmask 0x00ff0000
+#define bmask 0x0000ff00
+#define amask 0x000000ff
+#else
+#define rmask 0x000000ff
+#define gmask 0x0000ff00
+#define bmask 0x00ff0000
+#define amask 0xff000000
+#endif
+
 #endif
 
