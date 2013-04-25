3384d143ba000542e49b16bb2aaddb5b1a5bcb3e
lucas8 <luc.linux@mailoo.org>
2013-04-25 19:41:13 +0200

    Création de la classe ScoreBar : barre de score.

---
+++ src/scorebar.hpp
@@ -0,0 +1,31 @@
+
+#ifndef DEF_SCOREBAR
+#define DEF_SCOREBAR
+
+#include "config.hpp"
+#include <SDL.h>
+class Gui;
+class Snake;
+
+class ScoreBar
+{
+	public:
+		ScoreBar(const Gui* gui, Snake* p[max_players], int w, int h);
+		~ScoreBar();
+
+		void blitOn(SDL_Surface* dst, SDL_Rect pos);
+
+	private:
+		const int m_width;
+		const int m_height;
+		const Gui* m_gui;
+
+		Snake** m_players;
+		SDL_Surface* m_parts[max_players];
+		int m_actives; // Combien sont utilisés
+
+		void genPlayerPart(int p);
+};
+
+#endif
+
