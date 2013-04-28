dcbb31e677dd8fb69eceed827e460c2380467344
lucas8 <luc.linux@mailoo.org>
2013-04-28 12:43:35 +0200

    Ajout d'une classe de controler pour le clavier.

---
+++ src/keyboardcontroler.cpp
@@ -0,0 +1,81 @@
+
+#include "keyboardcontroler.hpp"
+#include "snake.hpp"
+
+int KeyboardControler::m_count = 0;
+
+	KeyboardControler::KeyboardControler()
+: m_valid(SDLK_UNKNOWN)
+{
+	++m_count;
+}
+
+KeyboardControler::~KeyboardControler()
+{
+	--m_count;
+}
+
+bool KeyboardControler::validEvent(const SDL_Event& ev) const
+{
+	if(m_count > 1) // Un seul clavier autorisé
+		return false;
+
+	if(ev.type != SDL_KEYDOWN)
+		return false;
+
+	switch(ev.key.keysym.sym)
+	{
+		case SDLK_UP:
+		case SDLK_DOWN:
+		case SDLK_RIGHT:
+		case SDLK_LEFT:
+			m_valid = ev.key.keysym.sym;
+			return true;
+		default:
+			return false;
+	};
+}
+
+bool KeyboardControler::stillValid(const SDL_Event& ev) const
+{
+	if(m_count > 1) // Ne devrait pas arriver
+		return false;
+	
+	if(ev.type != SDL_KEYUP)
+		return true;
+
+	if(ev.key.keysym.sym == m_valid)
+		return false;
+	else
+		return true;
+}
+
+bool KeyboardControler::processEvent(const SDL_Event& ev)
+{
+	if(m_count > 1) // Ne devrait pas arriver
+		return false;
+
+	if(ev.type != SDL_KEYDOWN)
+		return false;
+
+	switch(ev.key.keysym.sym)
+	{
+		case SDLK_UP:
+			m_mv = &Snake::moveUp;
+			break;
+		case SDLK_DOWN:
+			m_mv = &Snake::moveDown;
+			break;
+		case SDLK_RIGHT:
+			m_mv = &Snake::moveRight;
+			break;
+		case SDLK_LEFT:
+			m_mv = &Snake::moveLeft;
+			break;
+		default:
+			return false;
+	};
+	return true;
+}
+
+
---
+++ src/keyboardcontroler.hpp
@@ -0,0 +1,26 @@
+
+#ifndef DEF_KEYBOARDCONTROLER
+#define DEF_KEYBOARDCONTROLER
+
+#include "localcontroler.hpp"
+#include <SDL.h>
+
+class KeyboardControler : public LocalControler
+{
+	public:
+		KeyboardControler();
+		virtual ~KeyboardControler();
+
+		// Utilisés lors de la détection des périphériques
+		virtual bool validEvent(const SDL_Event& ev) const; // Teste si l'évènement peut être utilisé
+		virtual bool stillValid(const SDL_Event& ev) const; // Teste si le nouvel ev n'annule pas le premier
+
+		virtual bool processEvent(const SDL_Event& ev);
+
+	private:
+		static int m_count; // Ne peut y avoir qu'un seul clavier
+		mutable SDLKey m_valid; // ev de validation
+};
+
+#endif
+
